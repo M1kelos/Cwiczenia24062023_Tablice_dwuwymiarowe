0c4c6ae245bea2ac19bdda0ce71d3bf9db4fc8ba
M1kelos <128227021+M1kelos@users.noreply.github.com>
2023-06-24 20:04:42 +0200

    Add files via upload

---
+++ 01_Tablica_10x10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] tablica = new int[10, 10];
+        int liczba = 1;
+
+        for (int i = 0; i < tablica.GetLength(0); i++)
+        {
+            if (i % 2 == 0) // parzysty wiersz
+            {
+                for (int j = 0; j < tablica.GetLength(1); j++)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+            else // nieparzysty wiersz
+            {
+                for (int j = tablica.GetLength(1) - 1; j >= 0; j--)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+        }
+
+        // Wypisanie tablicy na ekranie
+        for (int i = 0; i < tablica.GetLength(0); i++)
+        {
+            for (int j = 0; j < tablica.GetLength(1); j++)
+            {
+                Console.Write(tablica[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 02_Slimak.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] tablica = new int[10, 10];
+        int liczba = 1;
+
+        int wiersze = tablica.GetLength(0);
+        int kolumny = tablica.GetLength(1);
+        int dol = 0;
+        int gora = wiersze - 1;
+        int lewo = 0;
+        int prawo = kolumny - 1;
+        int kierunek = 0;
+
+        while (dol <= gora && lewo <= prawo)
+        {
+            if (kierunek == 0)
+            {
+                for (int i = lewo; i <= prawo; i++)
+                {
+                    tablica[dol, i] = liczba;
+                    liczba++;
+                }
+                dol++;
+            }
+            else if (kierunek == 1)
+            {
+                for (int i = dol; i <= gora; i++)
+                {
+                    tablica[i, prawo] = liczba;
+                    liczba++;
+                }
+                prawo--;
+            }
+            else if (kierunek == 2)
+            {
+                for (int i = prawo; i >= lewo; i--)
+                {
+                    tablica[gora, i] = liczba;
+                    liczba++;
+                }
+                gora--;
+            }
+            else if (kierunek == 3)
+            {
+                for (int i = gora; i >= dol; i--)
+                {
+                    tablica[i, lewo] = liczba;
+                    liczba++;
+                }
+                lewo++;
+            }
+
+            kierunek = (kierunek + 1) % 4;
+        }
+
+        // Wypisanie tablicy na ekranie
+        for (int i = 0; i < tablica.GetLength(0); i++)
+        {
+            for (int j = 0; j < tablica.GetLength(1); j++)
+            {
+                Console.Write(tablica[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
